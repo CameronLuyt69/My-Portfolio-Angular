defc6a0a27c4e2cb78af8d3369d2c3f65b004c7f
Cameron <cameronluyt30@gmail.com>
2021-03-16 20:57:44 +0200

    feat(header): Brought resumeData into header and styled it

--- My-Portfolio-Angular/src/app/components/header/header.component.ts
+++ My-Portfolio-Angular/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResumeDataService } from '../../services/resume-data.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  resumeData: any;
+
+  constructor(private resumeDataServise: ResumeDataService) { }
 
   ngOnInit(): void {
+    this.getResumeData()
   }
 
+  getResumeData(): void {
+    this.resumeDataServise.getResumeData()
+    .subscribe(resumeData => this.resumeData = resumeData);
+  }
 }
